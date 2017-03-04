a0bd83f70941b1be345c44fb48a31db53834d44f
Lars Mogren <lars.mogren@gmail.com>
2017-03-04 13:36:55 +0100

    Adds new mirror image for arm64 because of https://github.com/minio/mc/issues/2047.

--- build.sh
+++ build.sh
@@ -6,30 +6,28 @@ if [ ! -f qemu-aarch64-static ]; then
   echo "downloading qemu ..."
   wget -q https://github.com/multiarch/qemu-user-static/releases/download/$QEMU_VERSION/qemu-aarch64-static
   chmod a+x qemu-aarch64-static
-  cp qemu-aarch64-static server
-  cp qemu-aarch64-static mc
-  echo "qemu done"
+  echo "qemu $QEMU_VERSION done"
 fi
 
-if [ ! -f ./server/minio ]; then
+if [ ! -f minio ]; then
   echo "downloading minio ..."
-  wget -q -O ./server/minio https://dl.minio.io/server/minio/release/linux-arm64/minio
-  chmod a+x ./server/minio
+  wget -q minio https://dl.minio.io/server/minio/release/linux-arm64/minio
+  chmod a+x minio
   echo "minio done"
 fi
 
-if [ ! -f ./mc/mc ]; then
+if [ ! -f mc ]; then
   echo "downloading mc ..."
-  wget -q -O ./mc/mc https://dl.minio.io/client/mc/release/linux-arm64/mc
-  chmod a+x ./mc/mc
+  wget -q https://dl.minio.io/client/mc/release/linux-arm64/mc
+  chmod a+x mc
   echo "mc done"
 fi
 
 echo "building minio server ..."
-docker build -t thenatureofsoftware/minio-arm64:latest ./server
+docker build -t thenatureofsoftware/minio-arm64:latest -f Dockerfile.minio .
 
 echo "building minio client ..."
-docker build -t thenatureofsoftware/mc-arm64:latest ./mc
+docker build -t thenatureofsoftware/mc-arm64:latest -f Dockerfile.mc .
 
 echo "building minio mirror ..."
-docker build -t thenatureofsoftware/mirror-arm64:latest ./mirror
+docker build -t thenatureofsoftware/mc-mirror-arm64:latest -f Dockerfile.mc-mirror .
