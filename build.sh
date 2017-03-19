884c943823d4a6f73c81749810b461ae2e2d6cb5
Lars Mogren <lars.mogren@gmail.com>
2017-03-19 11:51:17 +0100

    now uses multiarch/alpine

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 WORKDIR=$BASEDIR/.work
 mkdir -p $WORKDIR
 
-QEMU_VERSION=v2.8.0
+QEMU_VERSION=v2.8.1
 
 if [ ! -f $WORKDIR/manifest-tool ]; then
   if [ "Darwin" == $(uname) ]; then
@@ -19,15 +19,6 @@ if [ ! -f $WORKDIR/manifest-tool ]; then
   echo "manifest-tool done"
 fi
 
-if [ ! -f $WORKDIR/qemu-arm-static ]; then
-  echo "downloading qemu ..."
-  QEMU_URL="https://github.com/multiarch/qemu-user-static/releases/download/$QEMU_VERSION"
-  wget -q -O $WORKDIR/qemu-arm-static $QEMU_URL/qemu-arm-static
-  wget -q -O $WORKDIR/qemu-aarch64-static $QEMU_URL/qemu-aarch64-static
-  chmod a+x $WORKDIR/qemu-a*-static
-  echo "qemu $QEMU_VERSION done"
-fi
-
 if [ ! -f $WORKDIR/minio-arm ]; then
   echo "downloading minio ..."
   wget -q -O $WORKDIR/minio-arm https://dl.minio.io/server/minio/release/linux-arm/minio
@@ -38,22 +29,27 @@ fi
 
 if [ ! -f $WORKDIR/mc-arm ]; then
   echo "downloading mc ..."
+  #wget -q -O $WORKDIR/mc-arm https://dl.minio.io/client/mc/release/linux-arm/archive/mc
   wget -q -O $WORKDIR/mc-arm https://dl.minio.io/client/mc/release/linux-arm/mc
+
+  #wget -q -O $WORKDIR/mc-arm64 https://dl.minio.io/client/mc/release/linux-arm64/archive/mc
   wget -q -O $WORKDIR/mc-arm64 https://dl.minio.io/client/mc/release/linux-arm64/mc
+
+  #wget -q -O $WORKDIR/mc-amd64 https://dl.minio.io/client/mc/release/linux-amd64/archive/mc
   wget -q -O $WORKDIR/mc-amd64 https://dl.minio.io/client/mc/release/linux-amd64/mc
   chmod a+x $WORKDIR/mc-*
   echo "mc done"
 fi
 
 echo "building minio server ..."
-docker build -t thenatureofsoftware/minio-arm:latest -f Dockerfile.minio-arm .
-docker build -t thenatureofsoftware/minio-arm64:latest -f Dockerfile.minio-arm64 .
+docker build --no-cache -t thenatureofsoftware/minio-arm:latest -f Dockerfile.minio-arm .
+docker build --no-cache -t thenatureofsoftware/minio-arm64:latest -f Dockerfile.minio-arm64 .
 
 echo "building minio client ..."
-docker build -t thenatureofsoftware/mc-arm:latest -f Dockerfile.mc-arm .
-docker build -t thenatureofsoftware/mc-arm64:latest -f Dockerfile.mc-arm64 .
+docker build --no-cache -t thenatureofsoftware/mc-arm:latest -f Dockerfile.mc-arm .
+docker build --no-cache -t thenatureofsoftware/mc-arm64:latest -f Dockerfile.mc-arm64 .
 
 echo "building volumizr ..."
-docker build -t thenatureofsoftware/volumizr-arm:latest -f Dockerfile.volumizr-arm .
-docker build -t thenatureofsoftware/volumizr-arm64:latest -f Dockerfile.volumizr-arm64 .
-docker build -t thenatureofsoftware/volumizr-amd64:latest -f Dockerfile.volumizr-amd64 .
+docker build --no-cache -t thenatureofsoftware/volumizr-arm:latest -f Dockerfile.volumizr-arm .
+docker build --no-cache -t thenatureofsoftware/volumizr-arm64:latest -f Dockerfile.volumizr-arm64 .
+docker build --no-cache -t thenatureofsoftware/volumizr-amd64:latest -f Dockerfile.volumizr-amd64 .
